dbf05279cd998e3b4d69c011cd172af9d68f689b
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2018-01-25 15:41:47 -0500

    support for Promises

--- index.js
+++ index.js
@@ -24,38 +24,43 @@ var queuedReqs = [];
 
 BuzzAPI.prototype.post = function(resource, operation, data, callback) {
     if (openReqs >= 20) {
-        queuedReqs.push(arguments);
+        return new Promise((res, rej) => {
+            queuedReqs.push({'args': arguments, 'res': res, 'rej': rej});
+        });
     } else {
-        doPost(resource, operation, data, callback);
+        return doPost(resource, operation, data, callback);
     }
     return;
 };
 
 var doPost = function(resource, operation, data, callback) {
-    openReqs++;
-    if (_.isFunction(data)) {
-        callback = data;
-        data = {};
-    }
-    var myOpts = {};
-    myOpts.url = util.format('%s/apiv3/%s/%s', server, resource, operation);
-    data.handle = data.handle || util.format('from-%d@%s-rand%d', process.pid, os.hostname(), Math.floor(Math.random() * 32768));
-    myOpts.body = _.extend(data, options);
-    myOpts.json = true;
-    debug('Requesting %s', JSON.stringify(myOpts));
-    request.post(myOpts, function(err, res, body) {
-        if (err || body.api_error_info) {
-            if (body) {
-                return callback(new BuzzAPIError(err, body.api_error_info, body), null, body);
-            } else {
-                return callback(err);
-            }
-        } else if (options.api_request_mode === 'sync') {
-            return callback(null, body.api_result_data, body);
-        } else {
-            debug('Got messageId: %s', body.api_result_data);
-            return getResult(body.api_result_data, body.api_app_ticket, callback);
+    return new Promise((res, rej) => {
+        openReqs++;
+        if (_.isFunction(data)) {
+            callback = data;
+            data = {};
         }
+        var myOpts = {};
+        myOpts.url = util.format('%s/apiv3/%s/%s', server, resource, operation);
+        data.handle = data.handle || util.format('from-%d@%s-rand%d', process.pid, os.hostname(), Math.floor(Math.random() * 32768));
+        myOpts.body = _.extend(data, options);
+        myOpts.json = true;
+        debug('Requesting %s', JSON.stringify(myOpts));
+        request.post(myOpts, function(err, response, body) {
+            if (err || body.api_error_info) {
+                if (body) {
+                    let error = new BuzzAPIError(err, body.api_error_info, body);
+                    return callback ? callback(error, null, body) : rej(error);
+                } else {
+                    return callback ? callback(err) : rej(err);
+                }
+            } else if (options.api_request_mode === 'sync') {
+                return callback ? callback(null, body.api_result_data, body) : res(body.api_result_data);
+            } else {
+                debug('Got messageId: %s', body.api_result_data);
+                return res(getResult(body.api_result_data, body.api_app_ticket, callback));
+            }
+        });
     });
 };
 
@@ -63,57 +68,65 @@ var resolve = function(messageId) {
     openReqs--;
     debug('queued: %s  open: %s', queuedReqs.length, openReqs);
     if (queuedReqs[0]) {
-        doPost.apply(null, queuedReqs.pop());
+        let next = queuedReqs.pop();
+        return doPost.apply(null, next.args).then((result) => { next.res(result); }).catch((err) => { next.rej(err); });
     }
 };
 
 var getResult = function(messageId, ticket, initTime, callback) {
-    if (_.isFunction(initTime)) {
-        callback = initTime;
-        initTime = new Date();
-    } else if (new Date() - initTime > options.api_receive_timeout){
-        resolve(messageId);
-        return callback(new Error('Request timed out for: ' + messageId));
-    }
-    debug('Asking for result of %s', messageId);
-    request({
-        'url': util.format('%s/apiv3/api.my_messages', server),
-        'qs': {
-            'api_app_ticket': ticket,
-            'api_pull_response_to': messageId,
-            'api_receive_timeout': 5000
-        },
-        'json': true
-    }, function(err, res, body) {
-        if (res && res.attempts && res.attempts > 1) { debug('Request took multiple attempts %s', res.attempts); }
-        if (err || body.api_error_info || (body.api_result_data && body.api_result_data.api_error_info)) {
-            if (! body) {
+    return new Promise((res, rej) => {
+        if (_.isFunction(initTime) || initTime === undefined) {
+            callback = initTime;
+            initTime = new Date();
+        } else if (new Date() - initTime > options.api_receive_timeout){
+            resolve(messageId);
+            let err = new Error('Request timed out for: ' + messageId);
+            return callback ? callback(err) : rej(err);
+        }
+        debug('Asking for result of %s', messageId);
+        request({
+            'url': util.format('%s/apiv3/api.my_messages', server),
+            'qs': {
+                'api_app_ticket': ticket,
+                'api_pull_response_to': messageId,
+                'api_receive_timeout': 5000
+            },
+            'json': true
+        }, function(err, response, body) {
+            if (response && response.attempts && response.attempts > 1) { debug('Request took multiple attempts %s', response.attempts); }
+            if (err || body.api_error_info || (body.api_result_data && body.api_result_data.api_error_info)) {
+                if (! body) {
+                    resolve(messageId);
+                    return callback ? callback(new BuzzAPIError(err)) : rej(new BuzzAPIError(err));
+                }
+                if (body.api_error_info) {
+                    resolve(messageId);
+                    let err = new BuzzAPIError('BuzzApi returned error_info', body.api_error_info, body);
+                    return callback ? callback(err) : rej(err);
+                } else if (body.api_result_data) {
+                    resolve(messageId);
+                    let err = new BuzzAPIError('BuzzApi returned error_info', body.api_result_data.api_error_info, body);
+                    return callback ? callback(err) : rej(err);
+                } else {
+                    resolve(messageId);
+                    let error = new BuzzAPIError(err, body, body);
+                    return callback ? callback(error) : rej(error);
+                }
+            } else if (_.isEmpty(body.api_result_data)) {
+                // Empty result_data here means our data isn't ready, wait 1 to 5 seconds and try again
+                return setTimeout(function() {
+                    return getResult(messageId, body.api_app_ticket, initTime, callback);
+                }, Math.floor(Math.random() * (5000 - 1000) + 1000));
+            } else if (!body.api_result_data.api_result_data) {
                 resolve(messageId);
-                return callback(new BuzzAPIError(err));
-            }
-            if (body.api_error_info) {
-                resolve(messageId);
-                return callback(new BuzzAPIError('BuzzApi returned error_info', body.api_error_info, body));
-            } else if (body.api_result_data) {
-                resolve(messageId);
-                return callback(new BuzzAPIError('BuzzApi returned error_info', body.api_result_data.api_error_info, body));
+                let err = new BuzzAPIError('BuzzAPI returned an empty result, this usually means it timed out requesting a resource', {}, body);
+                return callback ? callback(err) : rej(err);
             } else {
                 resolve(messageId);
-                return callback(new BuzzAPIError(err, body, body));
+                debug('Completed %s in %sms', messageId, new Date() - initTime);
+                return callback ? callback(null, body.api_result_data.api_result_data, body) : res(body.api_result_data.api_result_data);
             }
-        } else if (_.isEmpty(body.api_result_data)) {
-            // Empty result_data here means our data isn't ready, wait 1 to 5 seconds and try again
-            return setTimeout(function() {
-                return getResult(messageId, body.api_app_ticket, initTime, callback);
-            }, Math.floor(Math.random() * (5000 - 1000) + 1000));
-        } else if (!body.api_result_data.api_result_data) {
-            resolve(messageId);
-            return callback(new BuzzAPIError('BuzzAPI returned an empty result, this usually means it timed out requesting a resource', {}, body));
-        } else {
-            resolve(messageId);
-            debug('Completed %s in %sms', messageId, new Date() - initTime);
-            return callback(null, body.api_result_data.api_result_data, body);
-        }
+        });
     });
 };
 
