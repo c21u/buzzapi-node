2a612ff31723247a1f3ce3cb50644421304bcdf8
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2023-10-09 10:22:18 -0400

    redact api_user_password in logs

--- buzzApiError.js
+++ buzzApiError.js
@@ -27,6 +27,7 @@ function sanitize(buzzApiRequest) {
   if (buzzApiRequest && buzzApiRequest.options && buzzApiRequest.options.body) {
     const body = JSON.parse(buzzApiRequest.options.body);
     body.api_app_password ? body.api_app_password = "[REDACTED]": null;
+    body.api_user_password ? body.api_user_password = "[REDACTED]": null;
     body.password_base64 ? body.password_base64 = "[REDACTED]": null;
     return {
       ...buzzApiRequest,
